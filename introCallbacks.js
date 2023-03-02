2900312c8b034402a3db47b0d3cbdb301d3b88a9
Kevin Chan <chankevin13@gmail.com>
2023-03-02 15:07:22 -0500

    setClock

---
+++ introCallbacks.js
@@ -0,0 +1,14 @@
+class Clock {
+    constructor(){
+        const currentTime = new Date();
+        this.hours = currentTime.getHours();
+        this.minutes = currentTime.getMinutes();
+        this.seconds = currentTime.getSeconds();
+
+    };
+
+    printTime() {
+        const time = `${this.hours}::${this.minutes}::${this.seconds}`;
+        console.log(time);
+    }
+}
