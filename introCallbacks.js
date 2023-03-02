a1961ba2148084af1597f3645bd84e98a029a534
Samuel Kim <samuelskim420@gmail.com>
2023-03-02 21:31:36 +0000

    finished clock

--- introCallbacks.js
+++ introCallbacks.js
@@ -1,15 +1,38 @@
 class Clock {
     constructor(){
+        debugger;
         const currentTime = new Date();
         this.hours = currentTime.getHours();
         this.minutes = currentTime.getMinutes();
         this.seconds = currentTime.getSeconds();
-
+        this.printTime();
+        // const that = this;
+        // const unboundTick = this._tick;
+        // const boundTick = unboundTick.bind(this);
+        // setInterval(boundTick, 1000);
+        setInterval(this._tick.bind(this), 1000);
     };
 
     printTime() {
         const time = `${this.hours}::${this.minutes}::${this.seconds}`;
         console.log(time);
+    };
+
+    _tick() {
+        debugger;
+        this.seconds ++
+        if (this.seconds === 60) {
+            this.seconds = 0;
+            this.minutes ++;
+            if (this.minutes === 60) {
+                this.minutes = 0;
+                this.hours ++;
+                if (this.hours === 24) {
+                    this.hours = 0;
+                }
+            }
+        }
+        this.printTime();
     }
 }
 
